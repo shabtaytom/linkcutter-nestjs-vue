db6ad9e31eb8ade4467ee3c4ce5d86b6586b8bf2
tom <tomshabtay@gmail.com>
2019-10-12 14:53:23 +0300

    added users module

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -3,13 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LinksModule } from './links/links.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/test'),
     LinksModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, UsersModule],
   providers: [AppService],
 })
 export class AppModule { }
